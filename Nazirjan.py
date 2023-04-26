efc3008d016b681c5931bf23fda060c249efa4b5
nazirjan0 <130881127+nazirjan0@users.noreply.github.com>
2023-04-26 10:29:02 +0430

    Add files via upload

---
+++ Nazirjan.py
@@ -0,0 +1,19 @@
+#.           #           #               ##                             
+#.        # #.         ##                ##                                         
+#.     #.   #.         ##                  ##                                       
+#.  #.      #.         ##                     ##            
+# #         #.         ##                        ##         
+
+
+
+
+
+
+
+#######################################################################
+# Said Nazir Danesh
+# MY GITHUB https://github.com/nazirjan0/Nazirjan
+# MY Telegram @afghan0511
+# MY Facebook https://www.facebook.com/profile.php?id=100088697089504
+{¥} fb account (€¥€]
+#FF000B
